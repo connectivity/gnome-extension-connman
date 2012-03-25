6f4a9cbfd447c5f4c7c36d5697bf3cab878cd673
Alok Barsode <alok.barsode@linux.intel.com>
2012-03-25 19:48:09 +0300

    extension: Add basic extension functions
    Add int/enable/disable functions.
    Add a basic panel icon.

---
+++ src/extension.js
@@ -0,0 +1,73 @@
+ /*
+  * Copyright (C) 2011 Intel Corporation. All rights reserved.
+  *
+  * This program is free software: you can redistribute it and/or modify
+  * it under the terms of the GNU General Public License as published by
+  * the Free Software Foundation, either version 3 of the License, or
+  * (at your option) any later version.
+  *
+  * This program is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  * GNU General Public License for more details.
+  *
+  * You should have received a copy of the GNU General Public License
+  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+  */
+
+const St = imports.gi.St;
+const Mainloop = imports.mainloop;
+const Main = imports.ui.main;
+const Lang = imports.lang;
+const Gio = imports.gi.Gio;
+const PopupMenu = imports.ui.popupMenu;
+const PanelMenu = imports.ui.panelMenu;
+const Gettext = imports.gettext;
+const Clutter = imports.gi.Clutter;
+
+const _ = Gettext.gettext;
+
+function ConnManager(metadata) {
+    this._init(metadata);
+}
+
+ConnManager.prototype = {
+    __proto__: PanelMenu.Button.prototype,
+
+    run: false,
+
+    _init: function(metadata) {
+        PanelMenu.Button.prototype._init.call(this, 0.0);
+        this.build_ui();
+    },
+
+    build_ui: function() {
+        this.icon = new St.Icon({
+            icon_type: St.IconType.SYMBOLIC,
+            style_class: "popup-menu-icon",
+            icon_name: "network-offline"
+        });
+
+        this.main_icon = new St.BoxLayout();
+        this.main_icon.add_actor(this.icon);
+
+        this.actor.add_actor(this.main_icon);
+    },
+
+    enable: function() {
+        this.run = true;
+        Main.panel._rightBox.insert_actor(this.actor, 0);
+        Main.panel._menus.addMenu(this.menu);
+    },
+
+    disable: function() {
+        this.run = false;
+        Main.panel._rightBox.remove_actor(this.actor);
+        Main.panel._menus.removeMenu(this.menu);
+    },
+}
+
+function init(metadata) {
+    global.log ('running ConnManager extension');
+    return new ConnManager(metadata);
+}
